5d1a5f8d622ba63eadc7ab0420f0a55717bdf2eb
ichord <chord.luo@gmail.com>
2014-09-10 10:37:38 +0800

    init
    with
    
    * golden rectangle
    * golden spiral section
    * golden spiral

---
+++ lib.js
@@ -0,0 +1,32 @@
+page = doc.currentPage()
+artboard = page.currentArtboard()
+canvas = artboard || page
+
+function makeRect (parent, width, height, x, y) {
+  layer = parent.addLayerOfType("rectangle")
+
+  layer.frame().width = width
+  layer.frame().height = height
+  layer.frame().x = x || 0
+  layer.frame().y = y || 0
+
+  layer.style().borders().addNewStylePart()
+  layer.style().border().position = 0
+  layer.style().border().thickness = 1
+  layer.style().border().color = [MSColor colorWithHex:"979797" alpha:1.0]
+
+  layer.style().fills().addNewStylePart()
+  layer.style().fill().color = [MSColor colorWithHex:"FFFFFF" alpha:0.0]
+  return layer
+}
+
+function makeCurve (parent, path) {
+  msPath = BCBezierPath.bezierPathWithNSBezierPath(bzPath)
+  layer = MSShapeGroup.shapeWithBezierPath(msPath)
+  layer.style().borders().addNewStylePart()
+  layer.style().border().position = 0
+  layer.style().border().thickness = 1
+  layer.style().border().color = [MSColor colorWithHex:"979797" alpha:1.0]
+
+  return parent.addLayer(layer)
+}
